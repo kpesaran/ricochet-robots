b2f7850bc26984e10ac9020a5af343564380cd6d
Keyan <Kpesaran@gmail.com>
2024-08-08 17:49:31 -0700

    added title, initial layout of ui, and center piece / one robot

--- frontend/main.js
+++ frontend/main.js
@@ -19,7 +19,7 @@ const sizes = {
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, .1, 100)
 camera.position.y = 20
-// camera.position.y = Math.PI * .5
+
 
 scene.add(camera)
 
@@ -32,8 +32,36 @@ const gridhelper = new THREE.GridHelper(16,16)
 scene.add(gridhelper)
 // Renderer 
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  alpha: true
 })
+
+// Center 
+
+const centerSquareGeom = new THREE.BoxGeometry(2, 1, 2) 
+const centerSquareMat = new THREE.MeshStandardMaterial({
+  color: 'grey'
+})
+
+const centerSquareMesh = new THREE.Mesh(centerSquareGeom, centerSquareMat)
+scene.add(centerSquareMesh)
+
+// Mock Pieces 
+
+
+const pieceGeom = new THREE.CylinderGeometry(.01, .3, 1) 
+const pieceMat = new THREE.MeshBasicMaterial({
+  color: 'red' 
+})
+const pieceMesh = new THREE.Mesh(pieceGeom, pieceMat)
+scene.add(pieceMesh)
+
+pieceMesh.position.set(4.5,.5,.5)
+
+// Lights 
+const ambientLight = new THREE.AmbientLight('#ffffff', .7)
+scene.add(ambientLight)
+// Render
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.render(scene,camera)
@@ -56,6 +84,8 @@ window.addEventListener('resize', () => {
 const tick = () => {
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
+  // For orbit controls damping
+  controls.update()
 }
 tick()
 
