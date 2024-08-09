51978bddc06c92460486eb92030e871ddd762cf5
Keyan <Kpesaran@gmail.com>
2024-08-08 18:54:27 -0700

    set up raycaster with mouse movements and mouse down

--- frontend/main.js
+++ frontend/main.js
@@ -40,7 +40,9 @@ const renderer = new THREE.WebGLRenderer({
 
 const centerSquareGeom = new THREE.BoxGeometry(2, 1, 2) 
 const centerSquareMat = new THREE.MeshStandardMaterial({
-  color: 'grey'
+  color: 'grey',
+  metalness: .1,
+  roughness: .5
 })
 
 const centerSquareMesh = new THREE.Mesh(centerSquareGeom, centerSquareMat)
@@ -49,14 +51,16 @@ scene.add(centerSquareMesh)
 // Mock Pieces 
 
 
-const pieceGeom = new THREE.CylinderGeometry(.01, .3, 1) 
-const pieceMat = new THREE.MeshBasicMaterial({
+const robotGeom = new THREE.CylinderGeometry(.01, .3, 1) 
+const robotMat = new THREE.MeshBasicMaterial({
   color: 'red' 
 })
-const pieceMesh = new THREE.Mesh(pieceGeom, pieceMat)
-scene.add(pieceMesh)
+const robotMesh = new THREE.Mesh(robotGeom, robotMat)
+scene.add(robotMesh)
 
-pieceMesh.position.set(4.5,.5,.5)
+robotMesh.position.set(4.5, .5, .5)
+
+
 
 // Lights 
 const ambientLight = new THREE.AmbientLight('#ffffff', .7)
@@ -64,7 +68,25 @@ scene.add(ambientLight)
 // Render
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.render(scene,camera)
+renderer.render(scene, camera)
+
+// Mouse 
+
+const mouse = {}
+window.addEventListener('mousemove', (event) => {
+  mouse.x =  (event.clientX / sizes.width)*2 - 1
+  mouse.y = -(event.clientY / sizes.height)*2 + 1
+})
+
+window.addEventListener('mousedown', (event) => {
+  console.log(event)
+  console.log(raycaster)
+})
+
+
+
+const raycaster = new THREE.Raycaster()
+console.log(raycaster)
 
 const clock = new THREE.Clock
 // window resize 
@@ -75,6 +97,8 @@ window.addEventListener('resize', () => {
 
   camera.aspect = sizes.width / sizes.height
   camera.updateProjectionMatrix()
+  // Cast ray from mouse 
+  
 
   // Update renderer
   renderer.setSize(sizes.width, sizes.height)
@@ -84,6 +108,24 @@ window.addEventListener('resize', () => {
 const tick = () => {
   renderer.render(scene, camera)
   window.requestAnimationFrame(tick)
+
+  // raycaster
+  raycaster.setFromCamera(mouse, camera)
+ 
+  if (robotMesh) {
+    const robotIntersects = 
+      raycaster.intersectObject(robotMesh)
+      
+    if (robotIntersects.length > 0) {
+      robotMesh.scale.set(1.5, 2, 1.5)
+      console.log(robotIntersects)
+    }
+
+    else {
+      robotMesh.scale.set(1,1,1)
+    }
+  }
+
   // For orbit controls damping
   controls.update()
 }
