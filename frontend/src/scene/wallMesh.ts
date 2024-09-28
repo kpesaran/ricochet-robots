f214046d266796a0cde908629d791d200a29b736
Keyan <Kpesaran@gmail.com>
2024-09-28 13:40:51 -0700

    Set up class to render wall in THREE.js

---
+++ frontend/src/scene/wallMesh.ts
@@ -0,0 +1,30 @@
+import * as THREE from 'three'
+
+
+
+export default class Wall {
+    // material: THREE.Material,
+    // geometry: THREE.BoxGeometry,
+    // mesh: THREE.Mesh
+
+    constructor() {
+        this.setGeometry()
+        this.setMaterial()
+        this.setMesh()
+    }
+
+    setGeometry() {
+
+    }
+
+    setMaterial() {
+
+    }
+
+    setMesh() {
+    
+    }
+
+
+
+}
