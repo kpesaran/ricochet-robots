6757840e4042150f58190e301a8c04765f212c93
Keyan <Kpesaran@gmail.com>
2024-09-28 14:54:38 -0700

    changed wall height

--- frontend/src/scene/wallPiece.ts
+++ frontend/src/scene/wallPiece.ts
@@ -15,7 +15,7 @@ export default class WallPiece {
     }
 
     setGeometry() {
-        this.geometry = new THREE.BoxGeometry(1, 0.5, 0.1);
+        this.geometry = new THREE.BoxGeometry(1, 0.4, 0.1);
     }
 
     setMaterial() {
@@ -28,19 +28,19 @@ export default class WallPiece {
         // Rotation based on direction 
        
         if (direction === Direction.North) {
-            this.mesh.position.set(pos.column - 7.5, 0.25, pos.row - 8);
+            this.mesh.position.set(pos.column - 7.5, 0.1, pos.row - 8);
             this.mesh.rotation.x = Math.PI ;
         }
         if (direction === Direction.South) {
-            this.mesh.position.set(pos.column - 7.5, 0.25, pos.row - 7);
+            this.mesh.position.set(pos.column - 7.5, 0.1, pos.row - 7);
             this.mesh.rotation.x = Math.PI ;
         }
         if (direction === Direction.West) {
-            this.mesh.position.set(pos.column - 8, 0.25, pos.row - 7.5);
+            this.mesh.position.set(pos.column - 8, 0.1, pos.row - 7.5);
             this.mesh.rotation.y = Math.PI * 0.5;
         }
         if (direction === Direction.East) {
-            this.mesh.position.set(pos.column - 7, 0.25, pos.row - 7.5);
+            this.mesh.position.set(pos.column - 7, 0.1, pos.row - 7.5);
             this.mesh.rotation.y = Math.PI * 0.5;
         }
 }
