6a2b0a4ab92266c828ade87c887fa3481f76fd9b
Keyan Pesaran <kpesaran@gmail.com>
2024-12-23 19:11:54 -0800

    All robots now moved with arrow keys and improved state management of the selectedrobot
    Robot-specific movement:
    -The `findMoves` method in the Board now takes a robotIndex parameter, allowing movements to be calculated for a specific robot rather than always assuming the target robot (index 0)
    -Adjusted the slideTargetRobot method to slideRobot(robotIndex, direction), enabling robot-specific movements
    
    SceneController Updates:
    - Added a selectedPiece property and set method for it
    - selectedPiece property set to target robot in the  `placeRobots` method
    - Refactored updateTargetRobot to updateRobot(robotIndex), allowing dynamic updates for any robot
    - modified raycasting logic to include all robots
    - mouse interactions now include target robot (to change mouse styling)
    
    InputController:
    
    - Keyboard handling now determines which bot to move based on the current selectedPiece
    - Refactored handleMouseDown to only change the robot selected instead of moving it
    - Removed code that handles moving pieces with mouse
    
    General Refactoring:
    
    - Cleaned up some unused comments
    - Deleted unneeded code
    - Improved consistency in accessing robot indices and positions

--- frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { SceneController } from './scene/sceneController.ts'
-import { GameController } from './game/gameController.ts'
-
-const canvas = document.querySelector('canvas.webgl')
-const gameController = new GameController()
