d2768d4b06af39c1b3b25a97c8f9091b00f688bd
Keyan Pesaran <kpesaran@gmail.com>
2025-01-01 11:17:14 -0800

    cleaned up css, added button hover effects for menu on win

--- frontend/src/util/uiController.ts
+++ frontend/src/util/uiController.ts
@@ -46,7 +46,7 @@ export default class UIController {
 
     toggleMainMenu(gameController: GameController) {
         const mainMenuElement = window.document.getElementById("menu-screen");
-        document.getElementById('menu-screen-final-score')!.textContent = `${this.moveCount}`
+        document.getElementById('menu-screen-final-score')!.textContent = `hi`
         
         if (mainMenuElement!.style.display === "flex") {
           gameController.hideMenu()
